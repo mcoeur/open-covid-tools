fc9710b7806df4a4b622ca1002b2561e2f0acdf7
Mallory COEUR <mallory.coeur@gmail.com>
2021-08-17 21:38:34 +0200

    [Proof of concept] Display QRCode

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Dimensions } from "react-native";
+
+import QRCode from "react-native-qrcode-svg";
 
 function HomeScreen({ navigation }) {
+  const width = Dimensions.get("window").width;
+  const padding = 50;
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
+      <View style={{ padding }}>
+        <QRCode value="proof of concept" size={width - padding * 2} />
+      </View>
       <Button
         title="Settings"
         onPress={() => navigation.navigate("Settings")}
